9aaeabefd80be89e8431a0f6ad27c28ec1125d55
Lorand Coc <lorand.83@gmail.com>
2026-04-06 20:06:53 +0300

    CTA button: just 'Coming Soon'

--- components/ComingSoonButton.tsx
+++ components/ComingSoonButton.tsx
@@ -69,7 +69,7 @@ export default function ComingSoonButton({
         <path d="M18 8A6 6 0 006 8c0 7-3 9-3 9h18s-3-2-3-9" />
         <path d="M13.73 21a2 2 0 01-3.46 0" />
       </svg>
-      Coming Soon &middot; Get Notified
+      Coming Soon
     </button>
   );
 }
